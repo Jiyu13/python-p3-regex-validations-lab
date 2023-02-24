0ee913eea29302304d2627ec143318b4e1da4189
Ziru Zou <jiyucoding@yahoo.com>
2023-02-24 11:50:16 -0500

    complete lab

--- lib/regex.py
+++ lib/regex.py
@@ -2,12 +2,12 @@ import re
 
 # NOTE: There are only a few tests included, so multiple solutions will work.
 # Feel free to encourage students to find oversights and add tests to this lab!
-
-name = r""
+3
+name = r"[A-Z][']*([A-z][\s\-']{0,1})+"
 name_regex = re.compile(name)
 
-phone_number = r""
+phone_number = r"([(]?[\d]{3}[)]?)([\s]?[\-]?)([\d]{3})([\-]?[\d]{4})"
 phone_regex = re.compile(phone_number)
 
-email_address = r""
+email_address = r"[^0-9][\w]+[.]?[\w]+@[\w]+[.]{1}[\w]+"
 email_regex = re.compile(email_address)
